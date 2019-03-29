dba35ea108d1acbf6ad108e6ad2d8130f83cdc13
flylzj <1449902124@qq.com>
2019-03-29 18:16:27 +0800

    修改点赞时间

--- coolq/plugins/big_finger.py
+++ coolq/plugins/big_finger.py
@@ -4,10 +4,10 @@ from aiocqhttp.exceptions import Error as CQHttpError
 from config import BIG_FINGER_GROUP_ID
 
 
-@nonebot.scheduler.scheduled_job('cron', minute='1-59/1')
+@nonebot.scheduler.scheduled_job('interval', hours="8, 21")
 async def _():
     bot = nonebot.get_bot()
     try:
-        await bot.send_group_msg(group_id=BIG_FINGER_GROUP_ID, message="记得点赞")
+        await bot.send_group_msg(group_id=BIG_FINGER_GROUP_ID, message="为了大拇指，记得去ta点赞")
     except CQHttpError as e:
         print(e)
