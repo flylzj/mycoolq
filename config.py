06498fdc39ecf9082937e96a3641ec2a9098da0c
flylzj <1449902124@qq.com>
2019-04-07 09:05:50 +0800

    暂时的提交

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 from nonebot.default_config import *
+from redis import ConnectionPool
 
 
 SUPERUSERS = {1449902124}
@@ -7,3 +8,10 @@ SUPERUSERS = {1449902124}
 BIG_FINGER_GROUP_ID = 654137970
 
 LIKED_ID = (2864158129, 1449902124)
+
+REDIS_CONFIG = {
+    "host": "redis",
+    "decode_response": True
+}
+
+POOL = ConnectionPool(**REDIS_CONFIG)
--- coolq/plugins/english_notice.py
+++ coolq/plugins/english_notice.py
@@ -1,15 +1,15 @@
 # coding: utf-8
 import nonebot
 from aiocqhttp.exceptions import Error as CQHttpError
-from config import LIKED_ID
+from config import BIG_FINGER_GROUP_ID, POOL
 from nonebot.log import logger
+import redis
 
 
-@nonebot.scheduler.scheduled_job('cron', hour="19")
+@nonebot.scheduler.scheduled_job('cron', hour="22, 23", minute='30')
 async def _():
     bot = nonebot.get_bot()
     try:
-        for id_ in LIKED_ID:
-            await bot.send_like(user_id=id_, times=10)
+        await bot.send_group_msg(group_id=BIG_FINGER_GROUP_ID, message="今天背单词了吗")
     except CQHttpError as e:
         logger.error(e, exc_info=True)
