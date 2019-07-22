d1494425c281743e5b0a85dec05d16d4058dc1d3
flythief <1449902124@qq.com>
2019-07-22 20:18:23 +0800

    完成游戏搜索功能

--- coolq/plugins/ns_jump/__init__.py
+++ coolq/plugins/ns_jump/__init__.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 from nonebot import on_command, CommandSession
-from .jump import search_game
+from .jump import search_game, get_game_detail
 
 
 @on_command('ns_jump', aliases=('ns', ), only_to_me=False)
@@ -14,7 +14,7 @@ async def ns_jump_command(session: CommandSession):
     await session.send(res)
 
 
-@ns_jump_command.args_parse
+@ns_jump_command.args_parser
 async def _(session: CommandSession):
     arg = session.current_arg_text.strip()
 
@@ -22,3 +22,34 @@ async def _(session: CommandSession):
         if arg:
             session.state['title'] = arg
         return
+
+    if not arg:
+        session.pause('要查询的游戏名称不能为空呢，请重新输入')
+
+    session.state[session.current_key] = arg
+
+
+@on_command('ns_jump', aliases=('nsgame', ), only_to_me=False)
+async def ns_jump_game_command(session: CommandSession):
+    appid = session.get('appid', prompt="请输入要查询的游戏作为参数")
+
+    try:
+        res = await get_game_detail(appid)
+    except Exception as e:
+        res = str(e)
+    await session.send(res)
+
+
+@ns_jump_game_command.args_parser
+async def _(session: CommandSession):
+    arg = session.current_arg_text.strip()
+
+    if session.is_first_run:
+        if arg:
+            session.state['appid'] = arg
+        return
+
+    if not arg:
+        session.pause('要查询的appid名称不能为空呢，请重新输入')
+
+    session.state[session.current_key] = arg
--- coolq/plugins/ns_jump/jump.py
+++ coolq/plugins/ns_jump/jump.py
@@ -2,6 +2,8 @@
 import aiohttp
 
 JUMP_SEARCH_API = "https://switch.vgjump.com/switch/gameDlc/list?title={}&offset=0&limit=10"
+JUMP_GAME_API = "https://switch.vgjump.com/switch/gameInfo?appid={}&" \
+                "system=Android%208.0.0&platform=android&fromName=search"
 
 
 async def search_game(title: str) -> str:
@@ -20,12 +22,39 @@ def parse_res(data: dict) -> str:
             return ""
         res = ""
         for game in games:
-            res += "游戏名:{}\n原价:{}\n现价:{}\n".format(
-                str(game.get("titleZh")) + str(game.get("title")),  # 这里的名字可能会为None
-                game.get("priceRaw") if game.get("priceRaw") else game.get("price"),
-                game.get("price")
+            res += "游戏名:{}\nappid：{}\n原价:{}\n现价:{}\n".format(
+                str(game.get("titleZh")),  # + str(game.get("title")),  # 这里的名字可能会为None
+                game.get("appid"),
+                game.get("priceRaw") if game.get("priceRaw") else "暂无",
+                game.get("price") if game.get("price") else "暂无"
             )
         return res
+
+
+async def get_game_detail(appid: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(JUMP_GAME_API.format(appid)) as r:
+            return parse_game_res(await r.json())
+
+
+def parse_game_res(data: dict) -> str:
+    if data.get("result").get("code") != 0:
+        return ""
+    data = data.get("data")
+    res = "游戏名：{}\n评价：{}\n玩家人数：{}\n".format(
+        data.get("game").get("titleZh"),
+        data.get("game").get("recommendLabel") if data.get("game").get("recommendLabel") else "暂无评价",
+        "{}-{}人".format(data.get("game").get("playersMin"), data.get("game").get("players"))
+    )
+    prices = data.get("prices")
+    prices.sort(key=lambda p: float(p.get("price")) if p.get("price") else 1)
+    for p in prices[:3]:
+        res += "{}区价格：￥{}".format(p.get("country"), p.get("price"))
+        if p.get("leftDiscount"):
+            res += "(优惠剩余{})\n".format(p.get("leftDiscount"))
+        else:
+            res += "\n"
+    return res
 
 
 if __name__ == '__main__':
