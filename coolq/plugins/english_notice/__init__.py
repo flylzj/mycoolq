8e67bf5c1c67e117fcef27186d7ca65ac30b343b
fly thief <1449902124@qq.com>
2019-10-12 16:10:15 +0800

    happy birthday

--- coolq/plugins/english_notice/__init__.py
+++ coolq/plugins/english_notice/__init__.py
@@ -6,17 +6,21 @@ from config import BIG_FINGER_GROUP_ID, ENGLISH_USERS
 from nonebot.log import logger
 
 
-# @nonebot.scheduler.scheduled_job('cron', hour="*", minute='*', second="1-59/30")  // debug time
-# @nonebot.scheduler.scheduled_job('cron', hour="22, 23", minute='1')
-# async def _():
-#     bot = nonebot.get_bot()
-#     try:
-#         records = get_recorded_today()
-#         recorded_users = [int(record.user_id) for record in records]
-#         for user in ENGLISH_USERS:
-#             if user not in recorded_users:
-#                 # await bot.send_group_msg(group_id=BIG_FINGER_GROUP_ID, message="今天背单词了吗")
-#                 msg = "今天还没有背单词~" + "[CQ:at,qq={}]".format(user)
-#                 await bot.send_group_msg(group_id=BIG_FINGER_GROUP_ID, message=msg)
-#     except CQHttpError as e:
-#         logger.error(e, exc_info=True)
+# @nonebot.scheduler.scheduled_job('cron', hour="*", minute='*', second="1-59/30")
+@nonebot.scheduler.scheduled_job('cron', hour="0", minute='0')
+async def _():
+    bot = nonebot.get_bot()
+
+    text = "阿向！这是刘崽给你准备的礼物，希望你能喜欢，生日快乐~\nhttps://www.bilibili.com/video/av70857887"
+    await bot.send_private_msg(user_id=1449902124, message=text)
+    # await bot.send_group_msg(group_id=BIG_FINGER_GROUP_ID, message=text)
+    # try:
+    #     records = get_recorded_today()
+    #     recorded_users = [int(record.user_id) for record in records]
+    #     for user in ENGLISH_USERS:
+    #         if user not in recorded_users:
+    #             # await bot.send_group_msg(group_id=BIG_FINGER_GROUP_ID, message="今天背单词了吗")
+    #             msg = "今天还没有背单词~" + "[CQ:at,qq={}]".format(user)
+    #             await bot.send_group_msg(group_id=BIG_FINGER_GROUP_ID, message=msg)
+    # except CQHttpError as e:
+    #     logger.error(e, exc_info=True)
