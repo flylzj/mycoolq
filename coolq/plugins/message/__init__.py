195ba5d8828f61e70f86fcc17235aa0b696dd04a
fly thief <1449902124@qq.com>
2020-03-02 10:05:34 +0800

    fix a bug

--- coolq/plugins/message/__init__.py
+++ coolq/plugins/message/__init__.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 import nonebot
 from nonebot.typing import Context_T
-from config import MANAGING_GROUPS
 from coolq.db.model.new_member_captcha import is_verifying
+from nonebot import Message
 
 bot = nonebot.get_bot()
 
@@ -12,7 +12,7 @@ async def handle_group_message(ctx: Context_T):
     user_id = ctx.get('user_id')
     if is_verifying(group_id, user_id):
         try:
-            from_message: str = ctx.get('message')
+            from_message: str = str(ctx.get('message'))
             if not from_message.startswith('验证码 ') or not from_message.strip('验证码 ').isdigit():
                 message = "请输入正确的验证格式为：验证码 123456"
                 await bot.send(ctx, message)
