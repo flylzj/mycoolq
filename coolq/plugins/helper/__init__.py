bd15cda0b1f679bf19757c0564d22f3532a83fc9
fly thief <1449902124@qq.com>
2020-03-03 09:34:12 +0800

    new feature

--- coolq/plugins/helper/__init__.py
+++ coolq/plugins/helper/__init__.py
@@ -1,10 +1,14 @@
 # coding: utf-8
 from nonebot import on_command, CommandSession
-import nonebot
 
 
 @on_command('help', aliases=("帮助"), only_to_me=False)
 def helper(session: CommandSession):
-    pass
+    message = '''
+    功能列表
+    lib---查看python标准库
+    roll---随机骰子点数
+    '''
+    session.send(message)
 
 
