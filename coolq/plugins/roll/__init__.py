1f93df495afb0e805d4ef8d813589fde5d53f155
fly thief <1449902124@qq.com>
2020-03-11 09:51:29 +0800

    负数点数

--- coolq/plugins/roll/__init__.py
+++ coolq/plugins/roll/__init__.py
@@ -12,7 +12,7 @@ async def roll_command(session: CommandSession):
     user_id = session.ctx.get('user_id')
     count = count_toady_roll(group_id=group_id, user_id=user_id)
     if count < 3 or group_id not in MANAGING_GROUPS:
-        roll = randint(1, 6)
+        roll = randint(-6, 6)
         message = "你肉到了{}点"
         insert_point(group_id=group_id, user_id=user_id, point=roll, roll_time=int(time.time()))
         await session.send(message=message.format(roll))
