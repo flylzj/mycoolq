9acf9b2d32c94922b7bba1945ab9d7242bc84e58
fly thief <1449902124@qq.com>
2020-03-25 16:28:18 +0800

    new table

--- create_db.py
+++ create_db.py
@@ -4,6 +4,7 @@ from config import ENGINE, SESSION
 from coolq.db.model.new_member_captcha import NewMemberCaptcha, Flag
 from coolq.db.model.python_lib_helper import PythonLibs, PythonLangRef
 from coolq.db.model.roll import RollHistory
+from coolq.db.model.count_day import Days
 from coolq.db.model import Base
 
 def init_db(new=False):
