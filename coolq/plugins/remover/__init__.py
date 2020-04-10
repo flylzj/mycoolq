9e90c40ccdcf43001a6163eacbac6a7d20ca85aa
fly thief <1449902124@qq.com>
2020-04-10 21:05:39 +0800

    m

--- coolq/plugins/remover/__init__.py
+++ coolq/plugins/remover/__init__.py
@@ -14,12 +14,12 @@ async def remove():
 
 async def _remove(bot: nonebot.NoneBot, group_id: str):
     members = await bot.get_group_member_list(group_id=group_id)
-    long_time_no_speak_func = lambda item: time.time() - item.get('last_sent_time') > 2592000 and item.get('role') == 'member'
+    long_time_no_speak_func = lambda item: time.time() - item.get('last_sent_time') > 2505600 and item.get('role') == 'member'
     long_time_no_speak_member = list(filter(long_time_no_speak_func, members))
     if long_time_no_speak_member:
         m = long_time_no_speak_member.pop()
         await bot.set_group_kick(group_id=group_id, user_id=m.get('user_id'), reject_add_request=False)
-        message = "群友 {} 由于超过2592000秒未发言已被移出该群".format(m.get('nickname'))
+        message = "群友 {} 由于超过2505600秒未发言已被移出该群".format(m.get('nickname'))
         await bot.send_group_msg(group_id=group_id, message=message)
 
 
