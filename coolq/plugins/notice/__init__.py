3329bac30a5dbe0a57e12a065cb7d0fc336dfaf5
flythief <1449902124@qq.com>
2021-04-05 17:21:33 +0800

    notice

--- coolq/plugins/notice/__init__.py
+++ coolq/plugins/notice/__init__.py
@@ -3,7 +3,7 @@ import nonebot
 from nonebot import on_command, CommandSession
 from coolq.db.model.notice import has_signed_today, sign as sign_to_db
 from coolq.util.coolq import at_someone
-from config import NOTICE_GROUP, NEED_SIGN_USER
+from config import  NEED_SIGN_USER
 
 
 @on_command("signed", aliases="打了")
@@ -17,13 +17,13 @@ async def sign(session: CommandSession):
             message += "打卡失败"
     else:
         message += "已经打过卡了"
-    await session.bot.send_group_msg(group_id=NOTICE_GROUP, message=message)
+    await session.bot.send_group_msg(group_id=NEED_SIGN_USER.get(user_id), message=message)
 
 
 @nonebot.scheduler.scheduled_job('cron', hour="8,17,21", minute="*")
 async def notice_sign():
-    for user_id in NEED_SIGN_USER:
+    for group_id, user_id in NEED_SIGN_USER:
         if not has_signed_today(user_id):
             bot = nonebot.get_bot()
             message = at_someone(user_id)
-            await bot.send_group_msg(group_id=NOTICE_GROUP, message=message)
+            await bot.send_group_msg(group_id=group_id, message=message)
