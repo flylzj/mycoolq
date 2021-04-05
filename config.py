1263ee02b9bef8d0d779b681f02151b18e8e66ba
flythief <1449902124@qq.com>
2021-04-05 17:06:05 +0800

    notice

--- config.py
+++ config.py
@@ -6,15 +6,23 @@ from sqlalchemy.orm import sessionmaker
 
 # db
 ENGINE = create_engine('sqlite:///./data/data.db', echo=True)
-
 SESSION = sessionmaker(bind=ENGINE)
-###
 
-## english
 
+# english
 SUPERUSERS = {1449902124}
 
-## remover manager
+# MY_NOTICE_GROUP
+NOTICE_GROUP = (
+    1076038734,
+)
+
+# Need Sign User
+NEED_SIGN_USER = (
+    646503792,
+)
+
+# remover manager
 MANAGING_GROUPS = [
     855508068,
     97795387
@@ -22,7 +30,7 @@ MANAGING_GROUPS = [
 
 QQ_GROUP_CAPTCHA_HINT_IMG = "https://flythief.cn/qq-group-captcha-hint.png"
 
-## python lib helper
+# python lib helper
 
 DOC_ROOT_URL = "https://docs.python.org/zh-cn/3.8/"
 
---
+++ coolq/plugins/notice/__init__.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+import nonebot
+from nonebot import on_command, CommandSession
+from coolq.db.model.notice import has_signed_today, sign as sign_to_db
+from coolq.util.coolq import at_someone
+from config import NOTICE_GROUP, NEED_SIGN_USER
+
+
+@on_command("signed", aliases="打了")
+async def sign(session: CommandSession):
+    user_id = session.ctx.get('user_id')
+    message = at_someone(user_id)
+    if not has_signed_today(user_id):
+        if sign_to_db(user_id):
+            message += "打卡成功"
+        else:
+            message += "打卡失败"
+    else:
+        message += "已经打过卡了"
+    await session.bot.send_group_msg(group_id=NOTICE_GROUP, message=message)
+
+
+@nonebot.scheduler.scheduled_job('cron', hour="8,17,21", minute="*")
+async def notice_sign():
+    for user_id in NEED_SIGN_USER:
+        if not has_signed_today(user_id):
+            bot = nonebot.get_bot()
+            message = at_someone(user_id)
+            await bot.send_group_msg(group_id=NOTICE_GROUP, message=message)
