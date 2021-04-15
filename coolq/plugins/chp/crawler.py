e6feecf203881e5d444d31cbfd99b48bb2cdb605
flythief <1449902124@qq.com>
2021-04-15 22:45:12 +0800

    chp

--- coolq/plugins/chp/crawler.py
+++ coolq/plugins/chp/crawler.py
@@ -1,12 +1,24 @@
 import aiohttp
+import json
+from nonebot.log import logger
 
 
 async def get_chp():
     chp_url = 'https://api.muxiaoguo.cn/api/caihongpi'
     async with aiohttp.ClientSession() as session:
-        async with session.get(url=chp_url) as r:
-            data = await r.json()
-            content = data['data']['comment']
-            if content:
-                return content
-            return "彩虹屁出问题了"
+        try:
+            async with session.get(url=chp_url) as r:
+                data = await r.text()
+                data = json.loads(data)
+                content = data['data']['comment']
+                if content:
+                    return content
+                return "彩虹屁出问题了"
+        except Exception as e:
+            logger.error("get_chp err" + str(e), exc_info=True)
+            return "无屁可放"
+
+
+if __name__ == '__main__':
+    import asyncio
+    print(asyncio.run(get_chp()))
