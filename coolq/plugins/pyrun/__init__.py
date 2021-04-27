f114b45b2b21ebfde7b530ae3decf61dbc09e710
flythief <1449902124@qq.com>
2021-04-27 14:49:01 +0800

    add

--- coolq/plugins/pyrun/__init__.py
+++ coolq/plugins/pyrun/__init__.py
@@ -7,7 +7,10 @@ from nonebot.typing import T_State
 
 def custom_exec(code):
     try:
-        return exec(code, {}, {})
+        res = exec(code, {}, {})
+        if not res:
+            return "结果为空"
+        return res
     except Exception as e:
         logger.error(f"custom_exec err {str(e)}")
         return "执行失败"
@@ -19,4 +22,5 @@ pyrun = on_command("pyrun")
 @pyrun.handle()
 async def run_code(bot: Bot, event: Event, state: T_State):
     arg_text = event.get_plaintext()
+    logger.error(f"arg text {arg_text}")
     await bot.send(event, custom_exec(arg_text))
