6e25469a7e62d2ffd3af3cd4ff8b4e2f24b762c0
flythief <1449902124@qq.com>
2021-05-12 23:09:23 +0800

    the boy

--- coolq/plugins/pretty_boy/handle.py
+++ coolq/plugins/pretty_boy/handle.py
@@ -5,6 +5,7 @@ from nonebot.typing import T_State
 from nonebot.adapters.cqhttp import Bot
 from nonebot.adapters.cqhttp import GroupMessageEvent
 from nonebot import require
+from nonebot.log import logger
 
 from .pick_boy import check_boy
 from . import config
@@ -14,6 +15,7 @@ async def base(group_id, bot_id):
     bot: Bot = nonebot.get_bots()[bot_id]
     boys = await bot.call_api('get_group_member_list', **{'group_id': group_id})
     sex, user_id = check_boy(boys=boys)
+    logger.info(f"the boy {user_id}")
     message = config.message + Message(f'[CQ:at,qq={user_id}]')
     return await bot.call_api('send_group_msg', **{
         'group_id': group_id,
@@ -27,6 +29,7 @@ scheduler = require("nonebot_plugin_apscheduler").scheduler
 @scheduler.scheduled_job("cron", hour="07", minute='52')
 async def handle_first_receive():
     # await base(group_id=config.group_id,bot_id=config.bot_id)
+    logger.info("start handle_first_receive")
     await base(group_id=config.group_id, bot_id=config.bot_id)
 
 pretty = on_command("靓仔", aliases={'今日靓仔', 'lz'}, priority=5)
