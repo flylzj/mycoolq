441ed3e9a50d9a9086d360adf98559e1fc23d5bc
flythief <1449902124@qq.com>
2022-01-07 11:18:51 +0800

    主动触发签到

--- mynonebot/coolq/plugins/sign_in_tools/__init__.py
+++ mynonebot/coolq/plugins/sign_in_tools/__init__.py
@@ -1,5 +1,8 @@
 # coding: utf-8
-from nonebot import get_driver, require
+from nonebot import get_driver, require, on_command
+from nonebot.permission import SUPERUSER
+from nonebot.typing import T_State
+from nonebot.adapters import Bot, Event
 
 from coolq.util.coolq import send_message
 from .config import Config
@@ -12,22 +15,38 @@ global_config = get_driver().config
 plugin_config = Config(**global_config.dict())
 
 
+class SignBox:
+
+    @staticmethod
+    async def t00ls_sign():
+        username = plugin_config.t00ls_username
+        login_form = {
+            'action': 'login',
+            'username': username,
+            'password': plugin_config.t00ls_password,
+            'questionid': plugin_config.t00ls_questionid,
+            'answer': plugin_config.t00ls_answer
+        }
+        t00ls = T00lsSign(username, login_form)
+
+        return await t00ls.sign_and_get_profile()
+
+
 # @scheduler.scheduled_job("cron", second="*/10") # debug
 @scheduler.scheduled_job("cron", hour="8")
 async def t00ls_sign_cron():
-    username = plugin_config.t00ls_username
-    login_form = {
-        'action': 'login',
-        'username': username,
-        'password': plugin_config.t00ls_password,
-        'questionid': plugin_config.t00ls_questionid,
-        'answer': plugin_config.t00ls_answer
-    }
-    t00ls = T00lsSign(username, login_form)
-    status, reason = await t00ls.login_and_sign()
+    await send_message(message_type="private", recipient_id="1449902124", message=SignBox.t00ls_sign())
 
-    message = "土司签到完成\nstatus:{}\nreason:{}".format(status, reason)
-    await send_message(message_type="private", recipient_id="1449902124", message=message)
 
+sign_command = on_command("sign", aliases={"签到"}, permission=SUPERUSER)
+
+
+@sign_command.handle()
+async def sign_command_handle(bot: Bot, event: Event, state: T_State):
+    args = str(event.get_message()).strip()
+    if not args:
+        pass
+    elif args == "土司":
+        await sign_command.finish(await SignBox.t00ls_sign())
 
 
--- mynonebot/coolq/plugins/sign_in_tools/sign_util.py
+++ mynonebot/coolq/plugins/sign_in_tools/sign_util.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 import aiohttp
+from nonebot.log import logger
 
 
 class BaseSign:
@@ -9,35 +10,80 @@ class BaseSign:
 class T00lsSign(BaseSign):
     login_url = "https://www.t00ls.cc/login.json"
     sign_url = "https://www.t00ls.cc/ajax-sign.json"
+    profile_url = "https://www.t00ls.net/members-profile.json"
+
+    SUCCESS = "success"
+
+    message_temp = "土司签到{}\n当前tubi：{}\n银行存款：{}\nreason\n{}"
+
+    TUBI_KEY = "extcredits2"  # tubi数量
+    TUBE_STORE_KEY = "extcredits3"  # 存款数量
 
     def __init__(self, username, login_form):
         self.username = username
         self.login_form = login_form
-        self.reason = "签到成功"
+        self.reason = ""
+        self.cookies = None
+        self.formhash = ""
 
     async def login(self):
         async with aiohttp.ClientSession() as session:
             async with session.post(self.login_url, data=self.login_form) as resp:
                 resp_json: dict = await resp.json()
-                if resp_json.get("status") != "success":
-                    self.reason = f'登录失败：{resp_json.get("message")}'
-                return resp_json.get("formhash"), resp.cookies
+                logger.debug("login resp ")
+                logger.debug(resp_json)
+                if resp_json.get("status") != self.SUCCESS:
+                    self.reason += f'登录失败：{resp_json.get("message")}'
+                    return
+                logger.debug("登录成功")
+                self.cookies = resp.cookies
+                self.formhash = resp_json.get("formhash")
 
-    async def sign(self, formhash, cookies):
+    async def _sign(self):
         sign_data = {
-            'formhash': formhash,
+            'formhash': self.formhash,
             'signsubmit': "true"
         }
-        async with aiohttp.ClientSession(cookies=cookies) as session:
+        async with aiohttp.ClientSession(cookies=self.cookies) as session:
             async with session.post(self.sign_url, data=sign_data) as resp:
                 resp_json: dict = await resp.json()
-                if resp_json.get("status") != "success":
-                    self.reason = f'签到失败：{resp_json.get("message")}'
-                return resp_json.get("status") == "success"
+                if resp_json.get("status") != self.SUCCESS:
+                    self.reason += f'签到失败：{resp_json.get("message")}'
+                return resp_json.get("status") == self.SUCCESS
 
-    async def login_and_sign(self):
-        formhash, cookies = await self.login()
-        if not formhash:
-            return False, self.reason
-        status = await self.sign(formhash, cookies)
-        return status, self.reason
+    async def _get_profile(self):
+        async with aiohttp.ClientSession(cookies=self.cookies) as session:
+            async with session.get(self.profile_url) as resp:
+                resp_json: dict = await resp.json()
+                if resp_json.get("status") != self.SUCCESS:
+                    self.reason += f"获取profile失败：{resp_json.get('message')}"
+                    return {}
+                return resp_json.get("memberinfo")
+
+    async def sign_and_get_profile(self):
+        await self.login()
+        if not self.formhash or not self.cookies:
+            return self.message_temp.format(
+                "失败",
+                0,
+                0,
+                self.reason
+            )
+        sign_status = await self._sign()
+        profile = await self._get_profile()
+        tubi_count = profile.get(self.TUBI_KEY)
+        tubi_store_count = profile.get(self.TUBE_STORE_KEY)
+
+        return self.message_temp.format(
+            "成功" if sign_status else "失败",
+            tubi_count,
+            tubi_store_count,
+            self.reason
+        )
+
+
+
+
+
+
+
