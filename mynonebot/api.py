5ec154413d718cc1344a96148915dda08cef4a08
flythief <1449902124@qq.com>
2022-09-24 20:17:22 +0800

    wecomchan

--- mynonebot/api.py
+++ mynonebot/api.py
@@ -5,6 +5,7 @@ import base64
 from pydantic import BaseModel
 
 from coolq.util.coolq import send_message
+from coolq.util.ftqq import send_msg_by_wecomchan
 
 
 class CheckchanCallbackModel(BaseModel):
@@ -31,19 +32,26 @@ async def send_msg(qq: str="", msg: str=""):
     return {}
 
 
-@app.post("/checkchan/callback/{recipient_id}")
+@app.post("/checkchan/callback/{recipient_id}/{wecom_info}")
 async def checkchan_callback(
         recipient_id: str,
+        wecom_info: str,
         id: str= Form(""),
         url: str= Form(""),
         value: str= Form(""),
         html: str= Form(""),
         link: str= Form("")
     ):
+    # wecom_info = "push_url###key"
+    wecom_info = base64.b64decode(wecom_info.encode()).decode()
+    wecom_info = wecom_info.split("###")
+    push_url = wecom_info[0]
+    key = wecom_info[1]
 
     msg = f"检测任务有新的通知\n" \
           f"值: {value}\n" \
           f"链接: {link}"
+    await send_msg_by_wecomchan(push_url, key, msg)
     await send_message(message_type="private", recipient_id=recipient_id, message=msg)
 
 
